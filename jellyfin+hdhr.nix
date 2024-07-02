e7bd7780d879b1a737d52e8bc40c31c2a3154da2
Caleb <git@cb-tech.me>
2024-07-02 16:49:27 -0400

    Update jellyfin+hdhr.nix

--- jellyfin+hdhr.nix
+++ jellyfin+hdhr.nix
@@ -1,5 +1,7 @@
 {
-  networking.firewall.extraCommands = "iptables -I nixos-fw -s 10.0.0.148 -p udp -j nixos-fw-accept"; 
+  # Configuration for the firewall to allow all UDP ports from the HDHomeRun device for streaming. (Replace XXX.XXX.XXX.XXX with your HDHomeRun IP)
+  networking.firewall.extraCommands = "iptables -I nixos-fw -s XXX.XXX.XXX.XXX -p udp -j nixos-fw-accept"; 
+  # Simple config for Jellyfin itself, no really complex config needed.
   services = {
     jellyfin = {
       enable = true;
